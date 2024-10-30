6eb18f7768e0a81b830672762e213b5a6976d20e
Bode F <bode.falade@outlook.com>
2024-10-29 21:36:43 -0500

    refactored the code bloc. added comments.

--- main.go
+++ main.go
@@ -9,49 +9,38 @@ const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func main() {
 	plainText := "HELLOWORLD"
-	fmt.Println("Plain text", plainText)
+	fmt.Println("Plain text:", plainText)
 	encrypted := encrypt(5, plainText)
-	fmt.Println("Encypted text: ", encrypted)
+	fmt.Println("Encrypted text:", encrypted)
 	decrypted := decrypt(5, encrypted)
-	fmt.Println("Decrypted text: ", decrypted)
+	fmt.Println("Decrypted text:", decrypted)
 }
 
-func hashLetterFn(key int, letter string) (result string) {
-	runes := []rune(letter)
-	lastLetterKey := string(runes[len(letter)-key : len(letter)])
-	leftOversLetter := string(runes[0 : len(letter)-key])
-
-	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOversLetter)
+func hashLetterFn(key int, letter string) string {
+	key = key % len(letter) // Ensure the key is within bounds
+	return letter[len(letter)-key:] + letter[:len(letter)-key]
 }
 
-func encrypt(key int, plainText string) (result string) {
+func encrypt(key int, plainText string) string {
 	hashLetter := hashLetterFn(key, originalLetter)
-	var hashedString = ""
-	findOne := func(r rune) rune {
-		pos := strings.Index(originalLetter, string([]rune{r}))
-		if pos != -1 {
-			letterPostion := (pos + len(originalLetter)) % len(originalLetter)
-			hashedString = hashedString + string(hashLetter[letterPostion])
+	return translateText(plainText, originalLetter, hashLetter)
+}
+
+func decrypt(key int, encryptedText string) string {
+	hashLetter := hashLetterFn(key, originalLetter)
+	return translateText(encryptedText, hashLetter, originalLetter)
+}
+
+// translateText maps characters from srcAlphabet to dstAlphabet.
+func translateText(text, srcAlphabet, dstAlphabet string) string {
+	mapping := make(map[rune]rune)
+	for i, char := range srcAlphabet {
+		mapping[char] = rune(dstAlphabet[i])
+	}
+	return strings.Map(func(r rune) rune {
+		if newChar, ok := mapping[r]; ok {
+			return newChar
 		}
 		return r
-	}
-
-	strings.Map(findOne, plainText)
-	return hashedString
-}
-
-func decrypt(key int, encryptedText string) (result string) {
-	hashLetter := hashLetterFn(key, originalLetter)
-	var hashedString = ""
-	findOne := func(r rune) rune {
-		pos := strings.Index(hashLetter, string([]rune{r}))
-		if pos != -1 {
-			letterPosition := (pos + len(originalLetter)) % len(originalLetter)
-			hashedString = hashedString + string(originalLetter[letterPosition])
-		}
-		return r
-	}
-	strings.Map(findOne, encryptedText)
-	return hashedString
-
+	}, text)
 }
